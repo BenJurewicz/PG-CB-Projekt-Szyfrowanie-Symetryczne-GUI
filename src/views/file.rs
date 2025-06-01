45a32ea8b3f17656288c95e35dea481e5e667d05
Benjamin Jurewicz <94708038+BenJurewicz@users.noreply.github.com>
2025-06-02 00:47:08 +0200

    Ex change

--- src/views/file.rs
+++ src/views/file.rs
@@ -8,7 +8,7 @@ use rfd::AsyncFileDialog;
 pub fn File() -> Element {
     // TODO Instead of vec for files,
     // TODO Have two variables: decrypted_file and encrypted_file
-    let mut files_uploaded = use_signal(|| Vec::new() as Vec<FileData>);
+    // let mut files_uploaded = use_signal(|| Vec::new() as Vec<FileData>);
 
     let decrypted_file: Signal<Option<FileData>> = use_signal(|| None);
     let encrypted_file: Signal<Option<FileData>> = use_signal(|| None);

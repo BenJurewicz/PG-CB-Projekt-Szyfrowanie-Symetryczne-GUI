6bf2c84009de461a5f9b1b88fcb8542fbcec8e6f
ZawrzykrajLidia <151872212+ZawrzykrajLidia@users.noreply.github.com>
2025-06-19 12:01:49 +0200

    Added labels

--- src/views/text.rs
+++ src/views/text.rs
@@ -10,17 +10,17 @@ pub fn Text() -> Element {
         }
 
         p {
-            class: "text-center mb-4",
+            class: "text-center mb-4 portrait:mx-2",
             "Encrypt and decrypt text using AES encryption in different modes."
         }
-        p {
-            class: "text-center portrait:mx-2 mb-4",
-            "Select an encryption mode and enter text to encrypt or decrypt."
-        }
         div {
-            class: "w-full max-w-7xl mx-auto px-4 sm:px-8 lg:px-16 landscape:mt-20",
+            class: "w-full flex-col max-w-7xl mx-auto px-4 sm:px-8 lg:px-16 landscape:mt-20",
+            p {
+                class: "portrait:text-center mb-4",
+                "Select encryption mode:"
+            }
             div {
-                class: "flex flex-row items-center justify-start mb-4 portrait:justify-center",
+                class: "flex flex-row items-center justify-start mb-4 portrait:justify-center max-[380px]:flex-col max-[376px]:gap-4",
                 input {
                     type: "radio",
                     name: "encryption_mode",
@@ -61,32 +61,62 @@ pub fn Text() -> Element {
             }
 
             div {
-                class: "flex justify-center mb-4",
-                input {
-                    type: "text",
-                    id: "pwd",
-                    name: "pwd",
-                    class: "input input-bordered py-2 text-base w-full portrait:max-w-[530px]",
-                    placeholder: "Password",
+                class: "flex flex-row items-center justify-between w-full landscape:mt-15 portrait:!flex-col portrait:!justify-center portrait:!items-center landscape:mb-10 portrait:mb-5",
+                // LEFT COLUMN
+                div {
+                    class: "flex flex-col w-full landscape:w-1/2 max-w-[530px]",
+                    label {
+                        for: "pwd",
+                        class: "mb-2 text-white",
+                        "Password:"
+                    }
+                    input {
+                        type: "text",
+                        id: "pwd",
+                        name: "pwd",
+                        class: "input input-bordered py-2 text-base w-full mb-4",
+                        placeholder: "Enter your password",
+                    }
+                    label {
+                        for: "encrypt-text",
+                        class: "mb-1 text-white",
+                        "Text to encrypt:"
+                    }
+                    textarea {
+                        id: "encrypt-text",
+                        class: "textarea textarea-bordered w-full",
+                        placeholder: "Enter text to encrypt...",
+                        rows: 10,
+                    }
                 }
-            }
-
-            div {
-                class: "flex flex-row items-center justify-between w-full portrait:!flex-col portrait:!justify-center portrait:!items-center",
-                textarea {
-                    class: "textarea textarea-bordered portrait:w-full landscape:w-1/2 max-w-[530px] w-full",
-                    placeholder: "Enter text to encrypt...",
-                    rows: 10,
-                }
+                // IMAGE
                 img {
                     src: "{EXCHANGE_SVG}",
                     alt: "Exchange Icon",
-                    class: "mx-8 w-8 h-8 portrait:my-4 landscape:mx-8 portrait:rotate-90",
+                    class: "w-8 h-8 landscape:mx-8 landscape:mt-25 portrait:my-2 portrait:mt-8 portrait:rotate-90",
                 }
-                textarea {
-                    class: "textarea textarea-bordered portrait:w-full landscape:w-1/2 max-w-[530px] w-full",
-                    placeholder: "Enter text to decrypt here...",
-                    rows: 10,
+                // RIGHT COLUMN
+                div {
+                    class: "flex flex-col w-full landscape:w-1/2 max-w-[530px]",
+                    // Invisible placeholders for alignment
+                    label {
+                        class: "mb-2 invisible portrait:hidden",
+                        "Password:"
+                    }
+                    input {
+                        class: "input input-bordered py-2 text-base w-full mb-4 invisible portrait:hidden",
+                    }
+                    label {
+                        for: "decrypt-text",
+                        class: "mb-1 text-white",
+                        "Text to decrypt:"
+                    }
+                    textarea {
+                        id: "decrypt-text",
+                        class: "textarea textarea-bordered w-full",
+                        placeholder: "Enter text to decrypt here...",
+                        rows: 10,
+                    }
                 }
             }
         }
